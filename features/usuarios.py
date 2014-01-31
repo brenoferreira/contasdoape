ae4a4e56455b2e08b2e91f8a159793a7b66a9bc6
Fernando Oliveira <fernando@fholiveira.com>
2014-01-31 01:10:52 -0200

    Criando factory de fake users

---
+++ features/usuarios.py
@@ -0,0 +1,26 @@
+usuarios = {
+    'Rorschach': { 
+        'nome': 'Walter Kovacs', 
+        'email': 'walter_lxhvauw_kovacs@tfbnw.net', 
+        'facebook': '100007600561574', 
+        'senha': '123@abc' 
+    },
+    'Comediante': { 
+        'nome': 'Edward Blake', 
+        'email': 'edward_wenibkj_blake@tfbnw.net', 
+        'facebook': '100007520438137', 
+        'senha': '123@abc' 
+    },
+    'Coruja': { 
+        'nome': 'Dan Dreiberg', 
+        'email': 'dan_udodmuj_dreiberg@tfbnw.net', 
+        'facebook': '100007562947479', 
+        'senha': '123@abc' 
+    },
+    'Ozymandias': { 
+        'nome': 'Adrian Veidt', 
+        'email': 'adrian_jutyery_veidt@tfbnw.net', 
+        'facebook': '100007519448299', 
+        'senha': '123@abc' 
+    }
+}
